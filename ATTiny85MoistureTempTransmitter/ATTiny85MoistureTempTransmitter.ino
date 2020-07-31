9cfb6522b0c3d81406302509134887dcb6094488
Odd Erik Midthaug <oesolberg@hotmail.com>
2020-07-31 11:34:59 +0200

    temp and moisture working ok

--- ATTiny85MoistureTempTransmitter/ATTiny85MoistureTempTransmitter.ino
+++ ATTiny85MoistureTempTransmitter/ATTiny85MoistureTempTransmitter.ino
@@ -17,14 +17,12 @@ const int AirValue = 593;   //you need to replace this value with Value_1
 const int WaterValue = 230;  //you need to replace this value with Value_2
 
 OneWire oneWire(ONE_WIRE_BUS);
-//OneWire TemperatureSensor(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 FineOffset tx(TX_PIN,3);
 
-byte saveADCSRA;                      // variable to save the content of the ADC for later. if needed.
-volatile byte counterWD = 0; // Count how many times WDog has fired. Used in the timing of the
-                                           // loop to increase the delay before the LED is illuminated. For example,
+byte saveADCSRA;             // variable to save the content of the ADC for later. if needed.
+volatile byte counterWD = 0; // Count how many times WDog has fired. 
 
 void DoBlink(int times=1);
 
@@ -35,58 +33,41 @@ void setup() {
   pinMode(MOISTURE_PIN,INPUT);
   StartSensorCurrent();
   sensors.begin();
-  delay(1000);
-
-  
+  delay(1000);  
   DoBlink(2); 
-  delay(1000);
-  
-  StopSensorCurrent();
-  
-  
+  delay(1000);  
+  StopSensorCurrent(); 
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
    if ( counterWD >= 3 )                 // if the WDog has fired more than 3 times......
   {
-    StartSensorCurrent();
-    
+    DoBlink(2);
+    StartSensorCurrent();    
     counterWD = 0;                        // reset the counterWD 
-    delay(1000);
-    //Reading of moisture
-    DoBlink(2);
-    int humidity=GetHumidity();
-    
-    //int humidity=1;
-    //humidity=88;
-    //Reading of temperature
-    DoBlink(3);
-    delay(1000);
+    delay(200);
+    //Reading of moisture    
+    int humidity=GetHumidity();    
+    //Reading of temperature    
+    delay(200);
     float temp=GetTemperature();
-    //DoBlink((int)temp);
-    //float temp=33.3;
-    //temp=33.3;
     //Sending of readResults
-    SendTemperatureAndMoisture(temp,humidity);    
-    
-    
-    DoBlink(2);
+    SendTemperatureAndMoisture(temp,humidity);        
     StopSensorCurrent();
-  } // end of if counterWD
- 
+    DoBlink(2);
+  } // end of if counterWD 
   sleepNow ();                          // then set up and enter sleep mode
-  
-
 }
 
-int GetHumidity(){
-  delay(500);
+int GetHumidity(){  
   int times=10;
   int soilMoistureValue = 0;
   for(int i=0;i<times;i++){
     soilMoistureValue = analogRead(MOISTURE_PIN);  
-    delay(200);
+    delay(100);
   }  
   if(soilMoistureValue>AirValue) soilMoistureValue=AirValue;
   if(soilMoistureValue<WaterValue) soilMoistureValue=WaterValue;
@@ -94,58 +75,33 @@ int GetHumidity(){
   return soilmoisturepercent;
 }
 
-//float GetTemperature(){
-//  byte i;
-//  byte data[12];
-//  int16_t raw;
-//  float t;
-//  
-//  TemperatureSensor.reset(); // reset one wire buss
-//  TemperatureSensor.skip(); // select only device
-//
-//  TemperatureSensor.write(0x44); // start conversion
-//  delay(1000); // wait for the conversion
-//  TemperatureSensor.reset();
-//  TemperatureSensor.skip();
-//  TemperatureSensor.write(0xBE); // Read Scratchpad
-//
-//  for ( i = 0; i < 9; i++) { // 9 bytes
-//  
-//    data[i] = TemperatureSensor.read();
-//  
-//    }
-//  raw = (data[1] << 8) | data[0];
-//  t = (float)raw / 16.0;
-//  return t;
-//  }
-
-float GetTemperature(){
-  //Serial.begin(9600); 
-  //sensors.reset();
+float GetTemperature(){  
   sensors.begin();
-  delay(1000);
+  float temp=-99;
+  delay(200);
   //Check if temp sensor is ok
   if (sensors.getDS18Count() == 0)
     DoErrorBlink();    
   else
-    DoBlink(1);
-  
-  sensors.requestTemperatures(); 
-  delay(1000);
-  float temp=sensors.getTempCByIndex(0);
+  {
+    //DoBlink(1);
+    sensors.requestTemperatures(); 
+    delay(200);
+    temp=sensors.getTempCByIndex(0);
+  }
   return temp;
 }
 
 void SendTemperatureAndMoisture(float temperature, int humidity){
   FineOffset tx(TX_PIN,3);
   delay(200);
-  tx.send(DEVICE_ID, temperature, humidity);
-  delay(1000);
+  tx.send(DEVICE_ID, temperature, humidity);  
 }
 
 void StartSensorCurrent(){
   digitalWrite(CURRENT_PIN,HIGH);
 }
+
 void StopSensorCurrent(){
   digitalWrite(CURRENT_PIN,LOW);
 }
